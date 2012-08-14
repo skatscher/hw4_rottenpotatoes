b1392aef42ee430460b5293932d8f362609d83fc
Stefan Katscher <skatscher@gmail.com>
2012-08-14 08:04:27 -0700

    add cucumber step definitions for movies

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,11 @@
+require "movie"
+
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    Movie.create!(movie)
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+  end
+end
